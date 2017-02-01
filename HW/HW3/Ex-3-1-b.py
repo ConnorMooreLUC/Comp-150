77290a9e7463e1a55ce738252db0b0adf3ec1663
Connor Moore <cmoore12@luc.edu>
2017-02-01 16:23:42 -0600

    Added First two parts of HW3

---
+++ HW/HW3/Ex-3-1-b.py
@@ -0,0 +1,40 @@
+'''
+TITLE: 3-1-b
+AUTHOR: Connor Moore
+DATE: 2.1.17
+DESCRIPTION: Prints the repetitive poem 10 times.
+MODIFICATION HISTORY AND OUTSIDE RESOURCES:
+Last Updated 2.1;
+Used section 1.5.2, and 1.7
+I added an empty line at the end of the poem,
+so that I can count the number of poems easier.
+'''
+
+poem = '''This is a repetitive poem
+It’s a poem that’s repetitive
+Repeating my repetitiveness
+In a repetitive poem
+I like using repetition
+I like writing poems
+So I’ll be repetitive
+And write a repetitive poem
+Poems can be repetitive
+Repetition can be a poem
+Repeating repetitiveness repeatedly
+In a poem in poetry that’s called a poem
+Which is repetitive
+Repetitive is this poem
+This poem is repetitive
+It’s repetition that’s repeated
+In poetry of the use of a poem
+Repeating poem repetitiveness
+Is repetitive of a repetition
+Repeated in a repeating, repetitive poem
+Poetry is so repetitively repeated
+Because a repetitive repeating repetitiveness repeated poem
+Is repetitive.
+
+'''
+print(10*poem)
+
+'''I did it!'''
