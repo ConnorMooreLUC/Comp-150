931ae6a99414dd0df52b9c391a58eee70cd5b70d
Connor Moore <cmoore12@luc.edu>
2017-03-26 23:20:56 -0500

    added fix to Ex-7.5.py

--- HW/HW7/Ex-7-5.py
+++ HW/HW7/Ex-7-5.py
@@ -17,16 +17,18 @@ def copyList(lista):
 
 def revList(listp):
     listq = []
-    for i in range(len(listp)):
-        listq.append(listp.pop())
+    listp1 = copyList(listp)
+    for i in range(len(listp1)):
+        listq.append(listp1.pop())
     return listq
 
 def main():
-    print(copyList([1,2,3,4,5,6]))
+    a=[1,2,3,4,5,6]
+    print(copyList(a))
     print(copyList([2,3,5]))
     print(copyList([7,11,13]))
     print(copyList([1,0,0,0,0,0,0]))
-    print(revList([1,2,3,4,5,6]))
+    print(revList(a))
     print(revList([6,5,4,3,2,1]))
     print(revList([7,11,13]))
     print(revList([1,2]))
